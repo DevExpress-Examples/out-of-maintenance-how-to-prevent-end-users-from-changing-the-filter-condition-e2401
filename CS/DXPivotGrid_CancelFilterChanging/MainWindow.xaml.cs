fc316f317f8a7684b342e7c3fb454813c8c1fe23
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_CancelFilterChanging/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+using DXPivotGrid_CancelFilterChanging.DataSet1TableAdapters;
+
+namespace DXPivotGrid_CancelFilterChanging {
+    public partial class MainWindow : Window {
+        DataSet1.ProductReportsDataTable productReportsDataTable =
+            new DataSet1.ProductReportsDataTable();
+        ProductReportsTableAdapter productReportsDataAdapter = new ProductReportsTableAdapter();
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = productReportsDataAdapter.GetData();
+        }
+        private void pivotGridControl1_FieldFilterChanging(object sender,
+                                        PivotFieldFilterChangingEventArgs e) {
+            if (Equals(e.Field, fieldCategoryName)) {
+                if ((e.Field.FilterValues.FilterType == FieldFilterType.Excluded &&
+                        e.Values.Contains("Beverages")) ||
+                    (e.Field.FilterValues.FilterType == FieldFilterType.Included &&
+                        !e.Values.Contains("Beverages"))) {
+                    MessageBox.Show("You are not allowed to hide the 'Beverages' value.");
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
---
+++ VB/DXPivotGrid_CancelFilterChanging/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+Imports DXPivotGrid_CancelFilterChanging.DataSet1TableAdapters
+
+Namespace DXPivotGrid_CancelFilterChanging
+	Partial Public Class MainWindow
+		Inherits Window
+		Private productReportsDataTable As New DataSet1.ProductReportsDataTable()
+		Private productReportsDataAdapter As New ProductReportsTableAdapter()
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = productReportsDataAdapter.GetData()
+		End Sub
+
+		Private Sub pivotGridControl1_FieldFilterChanging(ByVal sender As Object, _
+					ByVal e As PivotFieldFilterChangingEventArgs)
+			If Equals(e.Field, fieldCategoryName) Then
+				If (e.Field.FilterValues.FilterType = FieldFilterType.Excluded AndAlso _
+							e.Values.Contains("Beverages")) OrElse _
+							(e.Field.FilterValues.FilterType = FieldFilterType.Included AndAlso _
+							(Not e.Values.Contains("Beverages"))) Then
+					MessageBox.Show("You are not allowed to hide the 'Beverages' value.")
+					e.Cancel = True
+				End If
+			End If
+		End Sub
+	End Class
+End Namespace
