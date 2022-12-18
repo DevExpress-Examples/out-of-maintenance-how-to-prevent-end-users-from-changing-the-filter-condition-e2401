bc09f2bf518f5cb596b41419940499be77a779a4
DevExpressExampleBot <fakeEmail>
2022-12-18 20:16:37 +0400

    Source auto update [skip ci]

--- VB/DXPivotGrid_CancelFilterChanging/MainWindow.xaml.vb
+++ VB/DXPivotGrid_CancelFilterChanging/MainWindow.xaml.vb
@@ -1,29 +1,28 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports DXPivotGrid_CancelFilterChanging.DataSet1TableAdapters
 
 Namespace DXPivotGrid_CancelFilterChanging
-	Partial Public Class MainWindow
-		Inherits Window
-		Private productReportsDataTable As New DataSet1.ProductReportsDataTable()
-		Private productReportsDataAdapter As New ProductReportsTableAdapter()
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = productReportsDataAdapter.GetData()
-		End Sub
 
-		Private Sub pivotGridControl1_FieldFilterChanging(ByVal sender As Object, _
-					ByVal e As PivotFieldFilterChangingEventArgs)
-			If Equals(e.Field, fieldCategoryName) Then
-				If (e.Field.FilterValues.FilterType = FieldFilterType.Excluded AndAlso _
-							e.Values.Contains("Beverages")) OrElse _
-							(e.Field.FilterValues.FilterType = FieldFilterType.Included AndAlso _
-							(Not e.Values.Contains("Beverages"))) Then
-					MessageBox.Show("You are not allowed to hide the 'Beverages' value.")
-					e.Cancel = True
-				End If
-			End If
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Private productReportsDataTable As DataSet1.ProductReportsDataTable = New DataSet1.ProductReportsDataTable()
+
+        Private productReportsDataAdapter As ProductReportsTableAdapter = New ProductReportsTableAdapter()
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = productReportsDataAdapter.GetData()
+        End Sub
+
+        Private Sub pivotGridControl1_FieldFilterChanging(ByVal sender As Object, ByVal e As PivotFieldFilterChangingEventArgs)
+            If Object.Equals(e.Field, Me.fieldCategoryName) Then
+                If e.Field.FilterValues.FilterType = FieldFilterType.Excluded AndAlso e.Values.Contains("Beverages") OrElse e.Field.FilterValues.FilterType = FieldFilterType.Included AndAlso Not e.Values.Contains("Beverages") Then
+                    MessageBox.Show("You are not allowed to hide the 'Beverages' value.")
+                    e.Cancel = True
+                End If
+            End If
+        End Sub
+    End Class
 End Namespace
